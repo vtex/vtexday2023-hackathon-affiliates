a8dc189f6546a7155ee47ae4ad89562ab3f2c50b
fallwlite <me@fallwlite.com>
2023-06-06 09:32:15 -0300

    feat: organize transactions response

--- node/middlewares/Getnet/getTransactions.ts
+++ node/middlewares/Getnet/getTransactions.ts
@@ -21,12 +21,42 @@ export async function getTransactions(
       transaction_date_end: new Date(transactionDateEnd).toJSON(),
     });
 
-    if (get?.errors) {
+    if (get?.errors || !get?.status?.success) {
       throw new Error(get.errors);
     }
 
+    const list_transactions = []
+    if (!get.list_transactions.length) {
+      ctx.status = 204
+      ctx.body = 'No Content'
+    }
+
+    for (const transaction of get.list_transactions) {
+      const details = []
+      for (const detail of transaction.details) {
+        details.push({
+          payment_date: detail.payment_date,
+          subseller_rate_amount: detail.subseller_rate_amount,
+          subseller_rate_percentage: detail.subseller_rate_percentage,
+          number_installments: detail.number_installments,
+          installment: detail.installment,
+          installment_date: detail.installment_date,
+          installment_amount: detail.installment_amount
+        })
+      }
+
+      list_transactions.push({
+        order_id: transaction.summary.order_id,
+        transaction_date: transaction.summary.transaction_date,
+        confirmation_date: transaction.summary.confirmation_date,
+        transaction_sign: transaction.summary.transaction_sign,
+        card_payment_amount: transaction.summary.card_payment_amount,
+        details
+      })
+    }
+
     ctx.status = 200
-    ctx.body = get
+    ctx.body = list_transactions
 
     return ctx
   } catch (error) {
